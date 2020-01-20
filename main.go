526fdfbbd69fad26ded4a9e58401441f1d763058
Isan-Rivkin <isanrivkin@gmail.com>
2020-01-20 15:50:59 +0200

    TST: bug on purpose

--- main.go
+++ main.go
@@ -69,6 +69,8 @@ func main() {
 }
 
 func startKubernetesWatcher(configPath string) serverutil.StopFunc {
+	
+	error()
 
 	watcherConfig, err := config.LoadKubernetesConfig(configPath)
 	if err != nil {

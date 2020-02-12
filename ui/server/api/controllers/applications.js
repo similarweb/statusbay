01612dd925cd750ee811a9d382f89c295cdf56b3
liorbd <liorbd22@gmail.com>
2020-02-12 22:47:53 +0200

    DEV: [ui] fix the deployby filter. show 'no-data' message in table

--- ui/server/api/controllers/applications.js
+++ ui/server/api/controllers/applications.js
@@ -4,45 +4,46 @@ const urlPath = `/applications`;
 const querystring = require('querystring');
 
 axios.interceptors.request.use(request => {
-    return request
+  return request
 })
 
 const prepareParams = ({
-                           page = 0,
-                           rowsPerPage = 10,
-                           sortBy = "Name",
-                           sortDirection = "desc",
-                           cluster = false,
-                           from = false,
-                           to = false,
-                           name = false,
-                           nameSpace = false,
-                           status = false,
-                           userName = false,
-                            distinct = false
+                         page = 0,
+                         rowsPerPage = 10,
+                         sortBy = "Name",
+                         sortDirection = "desc",
+                         cluster = false,
+                         from = false,
+                         to = false,
+                         name = false,
+                         nameSpace = false,
+                         status = false,
+                         deployBy = false,
+                         distinct = false
                        } = {}) => {
-    const query = {
-        offset: page,
-        limit: rowsPerPage,
-        sortby: sortBy,
-        sortdirection: sortDirection,
-        cluster: cluster.join(','),
-        from,
-        to,
-        name,
-        namespace: nameSpace.join(','),
-        status: status.join(','),
-        userName,
-        distinct
-    };
-    return querystring.stringify(Object.fromEntries(Object.entries(query).filter(x => x[1] !== false && x[1] !== '')));
+  const query = {
+    offset: page,
+    limit: rowsPerPage,
+    sortby: sortBy,
+    sortdirection: sortDirection,
+    cluster: cluster.join(','),
+    from,
+    to,
+    name,
+    namespace: nameSpace.join(','),
+    status: status.join(','),
+    deployby: deployBy,
+    distinct
+  };
+  return querystring.stringify(Object.fromEntries(Object.entries(query)
+  .filter(x => x[1] !== false && x[1] !== '')));
 };
 module.exports = {
-    urlPath,
-    async getAll(params) {
-        return axios.get(`${config.apiBaseUrl}${urlPath}?${prepareParams(params)}`)
-    },
-    async getDistinct(params) {
-        return axios.get(`${config.apiBaseUrl}${urlPath}?${prepareParams(params)}&distinct=true`)
-    }
+  urlPath,
+  async getAll(params) {
+    return axios.get(`${config.apiBaseUrl}${urlPath}?${prepareParams(params)}`)
+  },
+  async getDistinct(params) {
+    return axios.get(`${config.apiBaseUrl}${urlPath}?${prepareParams(params)}&distinct=true`)
+  }
 };

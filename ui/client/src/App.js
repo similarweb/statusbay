71d097b7ed133711526c894c37fca5243a443fe5
liorbd <liorbd22@gmail.com>
2020-02-12 17:51:59 +0200

    DEV: [ui] change application page name

--- ui/client/src/App.js
+++ ui/client/src/App.js
@@ -103,7 +103,7 @@ function App() {
                   <Route exact path="/applications">
                     <Applications />
                   </Route>
-                  <Route exact path="/application/:appName">
+                  <Route exact path="/applications/:appName">
                     <ApplicationDeployments />
                   </Route>
                   <Route path="/application/:deploymentId">

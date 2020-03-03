2bc020cb09a39fdf6c6b5075e1f91824e22e0142
liorbd <liorbd22@gmail.com>
2020-03-03 15:12:03 +0200

    DEV: [ui] add PVC events

--- ui/client/src/components/EventsView/EventsViewLogs.js
+++ ui/client/src/components/EventsView/EventsViewLogs.js
@@ -6,24 +6,72 @@ import TableBody from '@material-ui/core/TableBody';
 import Table from '@material-ui/core/Table';
 import PropTypes from 'prop-types';
 import makeStyles from '@material-ui/core/styles/makeStyles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
 import Timeline from '../Timeline/Timeline';
 import NoData from '../Table/NoData';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   cell: {
     borderBottom: 0,
-    padding: 0
+    padding: 0,
+  },
+  tabs: {
+    minHeight: 24,
+  },
+  tab: {
+    minHeight: 36,
+  },
+  tabWrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  tabsCell: {
+    paddingTop: 0,
+    paddingBottom: 0,
+  },
+  marker: {
+    width: 10,
+    height: 10,
+    backgroundColor: theme.palette.error.main,
+    borderRadius: '50%',
+    marginRight: 12,
   },
 }));
 
-const EventsViewLogs = ({ logs }) => {
+const EventsViewLogs = ({
+  logs, onTabChange, selectedTab, tabs,
+}) => {
   const classes = useStyles();
-  const tableContent = logs.length > 0 ? <Timeline items={logs} maxHeight={400} /> : <NoData imageWidth={120} message="No events available" />;
+  const tableContent = logs.length > 0 ? <Timeline items={logs} maxHeight={400} />
+    : <NoData imageWidth={120} message="No events available" />;
   return (
     <Table size="small">
       <TableHead>
         <TableRow>
-          <TableCell>Events</TableCell>
+          <TableCell classes={{ root: classes.tabsCell }}>
+            <Tabs
+              value={selectedTab}
+              onChange={onTabChange}
+              className={classes.tabs}
+              variant="scrollable"
+              scrollButtons="auto"
+            >
+              {
+                tabs.map(({ name, error }) => (
+                  <Tab
+                    disableRipple
+                    classes={{ root: classes.tab, wrapper: classes.tabWrapper }}
+                    key={name}
+                    icon={<div className={error && classes.marker} />}
+                    label={name}
+                  />
+                ))
+              }
+            </Tabs>
+          </TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
@@ -46,10 +94,17 @@ EventsViewLogs.propTypes = {
     error: PropTypes.bool,
     content: PropTypes.string,
   })),
+  onTabChange: PropTypes.func.isRequired,
+  selectedTab: PropTypes.number.isRequired,
+  tabs: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    error: PropTypes.bool,
+  })),
 };
 
 EventsViewLogs.defaultProps = {
   logs: [],
+  tabs: [],
 };
 
 export default EventsViewLogs;
